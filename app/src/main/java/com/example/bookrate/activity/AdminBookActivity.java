6906ce81bdbe8943e673dbb3d5a3f1901333e920
Denisa-Manta <denisavalentina10@yahoo.com>
2025-05-28 18:24:04 +0300

    books read bug fix user activity

--- app/src/main/java/com/example/bookrate/activity/AdminBookActivity.java
+++ app/src/main/java/com/example/bookrate/activity/AdminBookActivity.java
@@ -1,6 +1,7 @@
 package com.example.bookrate.activity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +11,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.bookrate.R;
 import com.example.bookrate.adapter.AdminBookAdapter;
 import com.example.bookrate.model.Book;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.*;
 
 import java.util.ArrayList;
@@ -25,6 +27,10 @@ public class AdminBookActivity extends AppCompatActivity {
             "https://bookrate-4dc23-default-rtdb.europe-west1.firebasedatabase.app/")
             .getReference("books");
 
+    private final DatabaseReference usersRef = FirebaseDatabase.getInstance(
+            "https://bookrate-4dc23-default-rtdb.europe-west1.firebasedatabase.app/")
+            .getReference("users");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,18 +39,46 @@ public class AdminBookActivity extends AppCompatActivity {
         adminBooksRecyclerView = findViewById(R.id.adminBooksRecyclerView);
         adminBooksRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
         adapter = new AdminBookAdapter(this, bookList, (Book bookToDelete) -> {
             booksRef.child(bookToDelete.getId()).removeValue().addOnCompleteListener(task -> {
                 if (task.isSuccessful()) {
+                    Log.d("admin_debug", "Book deleted from admin side");
                     Toast.makeText(AdminBookActivity.this, "Book deleted", Toast.LENGTH_SHORT).show();
                     bookList.remove(bookToDelete);
                     adapter.notifyDataSetChanged();
                 } else {
+                    Log.d("admin_debug", "Book could not be deleted from admin side");
                     Toast.makeText(AdminBookActivity.this, "Failed to delete book", Toast.LENGTH_SHORT).show();
                 }
             });
-        });
 
+            usersRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot snapshot) {
+                    for (DataSnapshot userSnap : snapshot.getChildren()) {
+                        DatabaseReference userBookRef = userSnap.getRef()
+                                .child("bookStates")
+                                .child(bookToDelete.getId());
+
+                        userBookRef.removeValue().addOnCompleteListener(task -> {
+                            if (task.isSuccessful()) {
+                                Log.d("admin_debug", "Deleted book " + bookToDelete.getId() + " from user " + userSnap.getKey());
+                            } else {
+                                Log.d("admin_debug", "Failed to delete book " + bookToDelete.getId() + " from user " + userSnap.getKey());
+                            }
+                        });
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError error) {
+                    Log.e("admin_debug", "Failed to fetch users for cleanup: " + error.getMessage());
+                }
+            });
+
+        });
 
         adminBooksRecyclerView.setAdapter(adapter);
         fetchBooks();
