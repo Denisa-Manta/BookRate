2c310ad5282c48c7933fcf4f5cc9717bb70a67c6
Denisa-Manta <denisavalentina10@yahoo.com>
2025-05-28 20:51:11 +0300

    custom toolbar back button added

--- app/src/main/java/com/example/bookrate/activity/LoginActivity.java
+++ app/src/main/java/com/example/bookrate/activity/LoginActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.bookrate.R;
@@ -22,6 +23,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ImageView backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(v -> finish()); // 🔙 closes current activity and goes back
 
         mAuth = FirebaseAuth.getInstance();
 
